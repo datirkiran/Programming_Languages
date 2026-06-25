a032ed6aa1904797afaee7530d86326d93395fe7
Kiran Datir <datirkiran0@gmail.com>
2026-06-25 11:00:34 +0530

    initial commit

---
+++ C_Programming/Competitive/Assignments/Assignment-30/Program2.c
@@ -0,0 +1,27 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// Accept character from user. if it is capital than display all the character from the input
+// character till Z. if character is small than print all the character in reverse order till a. 
+// In other cases return directly.
+//  Input: S      Output: S T U V W X Y Z
+//  Input: g      Output: g f e d c b a
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Display(char ch)
+{
+
+}
+
+int main()
+{
+    char cValue = '\0';
+
+    printf("Enter the Character");
+    scanf("%c",&cValue);
+    
+    Display(cValue);
+    
+    return 0;
+}
+
